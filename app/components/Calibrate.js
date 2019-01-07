ef48e59307b24f5f2e451b566c713ace686706a6
Brandon Wong <bgwong@bu.edu>
2019-01-06 18:35:31 -0800

    Added custom numpad for data entry

--- app/components/Calibrate.js
+++ app/components/Calibrate.js
@@ -3,31 +3,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes.json';
-import NumPad from 'react-numpad';
+import NumPad from './calibrationInputs/NumPad';
+import Modal from 'react-responsive-modal';
+import styles from './modal-styling.css';
 
 export default class ODcalInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
+  state = {
+   open: false,
+ };
 
-    this.handleChange = this.handleChange.bind(this);
-  }
+ onOpenModal = () => {
+   this.setState({ open: true });
+ };
 
-  handleChange(event){
-    event = event.substring(0, event.length - 1);
-    console.log(event);
-    this.setState({value: event});
-  };
+ onCloseModal = () => {
+   this.setState({ open: false });
+ };
+
 
   render() {
+    const { open } = this.state;
+
     return (
       <div>
-        <NumPad.Number
-            value={this.state.value}
-            onChange={this.handleChange}
-            label={'Sample 1'}
-            position={'center'}
-        />
+        <p className="testText"> fasdfasdfdsa</p>
+        <button onClick={this.onOpenModal}>Open modal</button>
+        <Modal
+          open={open}
+          onClose={this.onCloseModal}
+          center
+          classNames={{
+             closeButton: styles.customcloseButton,
+             modal: styles.customModal,
+             overlay: styles.customOverlay,
+           }}>
+
+          <NumPad />
+        </Modal>
       </div>
 
     );
