0c639dcf601ec95af82dcfe9731b81440e25db92
Brandon Wong <bgwong@bu.edu>
2019-03-11 11:14:57 -0700

    Added experiment script manager back to App

---
+++ app/components/python-shell/hello.py
@@ -0,0 +1,8 @@
+import sys
+
+
+
+
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
---
+++ app/components/python-shell/hello1.py
@@ -0,0 +1,8 @@
+import sys
+
+import time
+
+while (1):
+    print('Hello 1 from Python!')
+    sys.stdout.flush()
+    time.sleep(2)
