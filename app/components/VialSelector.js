9ef15993d8c75749fcba82e4296ef3550421a8e3
Zachary Heins <zackheins@gmail.com>
2019-09-03 16:47:34 -0400

    Calibration updates for new cal formats

--- app/components/VialSelector.js
+++ app/components/VialSelector.js
@@ -53,7 +53,7 @@ class List extends Component {
       <div style={{width: 560}}>
         <div className="centered">
           {this.props.items.map((item) => (
-            <SelectableAlbum key={item.vial} vial={item.vial} selected={item.selected} od={item.od135} temp={item.temp}/>
+            <SelectableAlbum key={item.vial} vial={item.vial} selected={item.selected} od={item.od} temp={item.temp}/>
           ))}
         </div>
       </div>
