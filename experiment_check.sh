a4d92f763da2a331854d70279192476b8a221961
Ezira Yimer Wolle <ezirayw@bu.edu>
2020-04-16 23:59:57 -0400

    Adjusted bash script and added config file

--- experiment_check.sh
+++ experiment_check.sh
@@ -4,24 +4,26 @@
 #Processes in RUNNING are then checked against runningExpts.txt file, which describes any experiments either running through the eVOLVER app or those that were running before app closed
 #Experiments found in runningExpts.txt but not in RUNNING are then re-launched
 
+# Edit path to runningExpts.txt via config.sh file
+
+source config.sh
+
 IFS=$'\n'
-RUNNING=($(ps -ax | grep "eVOLVER.py" | tr -s " " | cut -d " " -f 5-))
-length=${#RUNNING[@]}
-input="./runningExpts.txt"
+running=($(ps -ax | grep "eVOLVER.py" | tr -s " " | cut -d " " -f 5-))
+length=${#running[@]}
 while IFS= read -r line; do
   temp=0
   for (( i = 1; i < $length; i++ )); do
-    if [ "$line" != "${RUNNING[i]}" ]; then
+    if [ "$line" != "${running[i]}" ]; then
       temp+=1
     fi
   done
-  if [ $temp -eq $((${#RUNNING[@]} - 1)) ]; then
+  if [ $temp -eq $((${#running[@]} - 1)) ]; then
     echo "Relaunching Experiment"
     experiment=$(echo $line | sed "s/p {/p '{/" | sed "s/0}/0}'/" | sed "s/-c n/-c y/")
     filedirname=$(echo $experiment | cut -d " " -f 1,2)
     args=$(echo $experiment | cut -d " " -f 3-)
-    ARGS=($(echo $args | tr " " "\n"))
-    python3 $filedirname ${ARGS[0]} ${ARGS[1]} ${ARGS[2]} ${ARGS[3]} ${ARGS[4]} ${ARGS[5]} ${ARGS[6]} ${ARGS[7]} ${ARGS[8]} ${ARGS[9]} ${ARGS[10]} ${ARGS[11]} ${ARGS[12]} ${ARGS[13]} ${ARGS[14]} ${ARGS[15]} &
+    args=($(echo $args | tr " " "\n"))
+    python3 $filedirname ${args[0]} ${args[1]} ${args[2]} ${args[3]} ${args[4]} ${args[5]} ${args[6]} ${args[7]} ${args[8]} ${args[9]} ${args[10]} ${args[11]} ${args[12]} ${args[13]} ${args[14]} ${args[15]} &
   fi
-done < "$input"
-done
+done < "$RUNNING_EXPTS_PATH"
