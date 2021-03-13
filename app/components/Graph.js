f0f4a596a31a17558dacc4a8a41786bc95575ba1
Ezira Yimer Wolle <36025985+ezirayw@users.noreply.github.com>
2021-03-13 12:54:22 -0500

    App log viewer (#141)
    * Adding log viewer. Viewer checks if dpu log exists and tails it for live updates
    
    * PR changes
    
    * Remove whitespace
    
    Co-authored-by: Zachary Heins <zackheins@gmail.com>

--- app/components/Graph.js
+++ app/components/Graph.js
@@ -8,6 +8,7 @@ import {FaArrowLeft} from 'react-icons/fa';
 import VialArrayGraph from './graphing/VialArrayGraph';
 import VialArrayBtns from './graphing/VialArrayBtns';
 import VialMenu from './graphing/VialMenu';
+import AceEditor from 'react-ace';
 
 const { dialog } = require('electron').remote
 
@@ -15,9 +16,10 @@ var path = require('path');
 var os = require('os');
 var zipdir = require('zip-dir');
 var fs = require('fs');
+var Tail = require('tail').Tail;
+var readline = require('readline');
 
 const styles = {
-
 };
 
 const ymaxChoicesOD = ['0.1', '0.5', '1.0', '2.0'];
@@ -40,11 +42,42 @@ class Graph extends React.Component {
       parameter: 'OD',
       parameterTitle: 'PARAMETER:',
       xaxisName: xAxisNameOD,
-      activePlot: 'ALL'
+      activePlot: 'ALL',
+      logToggleText: 'VIEW LOGS',
+      logToggleOptions: ['VIEW GRAPH', 'VIEW LOGS'],
+      logToggleState: true,
+      logData: ''
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
+    var options = {fromBeginning: false, follow: true, nLines: 200}
+    var logFilename = path.join(this.props.exptDir, 'data', 'evolver.log');
+
+    if (fs.existsSync(logFilename)) {
+      var tail = new Tail(logFilename, options);
+      const file = readline.createInterface({
+        input: fs.createReadStream(logFilename)
+        });
+      var reverseFile = '';
+      file.on('line', function(line){
+        reverseFile = line + '\n' + reverseFile;
+        }.bind(this));
+      file.on('close', function(line){
+        this.setState({logData: reverseFile});
+        }.bind(this));
+
+      tail.on("line", function(data) {
+        var logData = this.state.logData;
+        logData = data + '\n' + logData;
+        this.setState({logData: logData});
+      }.bind(this));
+      tail.on("error", function(error) {
+        console.log('ERROR: ', error);
+      });
+    } else {
+      this.setState({logData: 'No log file found'})
+    };
   }
 
   handleYmax = event => {
@@ -95,7 +128,7 @@ class Graph extends React.Component {
     })
   }
 
-  downloadData = () => {    
+  downloadData = () => {
     var pathToDownload;
     var isWin = os.platform() === 'win32';
     if (!isWin) {
@@ -105,8 +138,14 @@ class Graph extends React.Component {
       pathToDownload = dialog.showOpenDialog({properties: ['openDirectory', 'promptToCreate']});
     }
 
-    var zipFilename = path.join(pathToDownload[0], path.basename(this.props.exptDir) + '.zip');      
+    var zipFilename = path.join(pathToDownload[0], path.basename(this.props.exptDir) + '.zip');
     zipdir(this.props.exptDir, {saveTo: zipFilename});
+  }
+
+  toggleLog = () => {
+    var logToggleState = !this.state.logToggleState;
+    var logToggleText = logToggleState ? this.state.logToggleOptions[1] : this.state.logToggleOptions[0];
+    this.setState({logToggleState: logToggleState, logToggleText: logToggleText});
   }
 
   handleActivePlot = (event) => {
@@ -114,19 +153,34 @@ class Graph extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
       var exptName = path.basename(this.props.exptDir);
+      var dataDisplay = this.state.logToggleState ?
+        <VialArrayGraph
+            parameter={this.state.parameter}
+            exptDir={this.props.exptDir}
+            activePlot = {this.state.activePlot}
+            ymax={this.state.ymax}
+            timePlotted={this.state.timePlotted}
+            downsample = {this.state.downsample}
+            xaxisName = {this.state.xaxisName}/> :
+        <div className="logViewer"><AceEditor
+            value={this.state.logData}
+            width='750px'
+            height='570px'
+            mode="elixer"
+            theme="terminal"
+            readOnly={true}
+            name="logViewer"
+            showGutter={false}
+            setOptions={{autoScrollEditorIntoView:true}}
+            editorProps={{$blockScrolling: true}}/></div>
+
     return (
       <div>
         <Link className="backHomeBtn" style={{zIndex: '10', position: 'absolute', top: '5px', left: '-20px'}} id="experiments" to={{pathname:routes.EXPTMANAGER, socket: this.props.socket, logger:this.props.logger}}><FaArrowLeft/></Link>
                 <h4 className="graphTitle">{exptName}</h4>
-        <VialArrayGraph
-          parameter={this.state.parameter}
-          exptDir={this.props.exptDir}
-          activePlot = {this.state.activePlot}
-          ymax={this.state.ymax}
-          timePlotted={this.state.timePlotted}
-          downsample = {this.state.downsample}
-          xaxisName = {this.state.xaxisName}/>
+                {dataDisplay}
         <div style={{position: 'absolute', top: '100px', left: '-10px'}}>
           <VialArrayBtns
             labels={this.state.parameterChoices}
@@ -147,7 +201,10 @@ class Graph extends React.Component {
             onSelectRadio={this.handleYmax}/>
         </div>
         <VialMenu onSelectGraph={this.handleActivePlot}/>
-        <button className={"downloadButton"} onClick={this.downloadData}>DOWNLOAD</button>
+        <div className="dataActionButtons">
+          <button className={"dataActionButton"} onClick={this.downloadData}>DOWNLOAD</button>
+          <button className={"dataActionButton"} onClick={this.toggleLog}>{this.state.logToggleText}</button>
+        </div>
       </div>
 
     );
